200225f9e88baf2ba60de6b06fd442f4715c58c8
SantiagoBorgna <santiborgna5@gmail.com>
2026-01-29 21:25:16 -0300

    Preparacion db

---
+++ src/main/java/com/alura/forohub/domain/topico/Topico.java
@@ -0,0 +1,52 @@
+package com.alura.forohub.domain.topico;
+
+import com.alura.forohub.domain.curso.Curso;
+import com.alura.forohub.domain.usuario.Usuario;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Table(name = "topicos")
+@Entity(name = "Topico")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Topico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // evita tópicos duplicados con el mismo título
+    @Column(unique = true)
+    private String titulo;
+
+    private String mensaje;
+
+    private LocalDateTime fechaCreacion = LocalDateTime.now();
+
+    @Enumerated(EnumType.STRING)
+    private StatusTopico status = StatusTopico.NO_RESPONDIDO;
+
+    // RELACIÓN CON USUARIO
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "autor_id")
+    private Usuario autor;
+
+    // RELACIÓN CON CURSO
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "curso_id")
+    private Curso curso;
+
+    public Topico(String titulo, String mensaje, Usuario autor, Curso curso) {
+        this.titulo = titulo;
+        this.mensaje = mensaje;
+        this.autor = autor;
+        this.curso = curso;
+    }
+}
